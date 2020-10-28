d4967e939a89dde9947a8d42f284583c4a67fb96
Arjun RK <arradhak@microsoft.com>
2020-10-28 13:10:36 +0530

    merge commit test

--- AzureDevops/Models/ErrorViewModel.cs
+++ AzureDevops/Models/ErrorViewModel.cs
@@ -5,7 +5,7 @@ namespace AzureDevops.Models
     public class ErrorViewModel
     {
         public string RequestId { get; set; }
-
+        // Merge commit.
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
 }
